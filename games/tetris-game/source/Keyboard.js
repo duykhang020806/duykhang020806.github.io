f0ecec604bd19752f14bc588951221a2c5cd6c37
duykhang020806 <satoohibiki@gmail.com>
2025-04-21 13:07:07 +0700

    Update Keyboard.js

--- games/tetris-game/source/Keyboard.js
+++ games/tetris-game/source/Keyboard.js
@@ -1,6 +1,7 @@
 class Keyboard {
     constructor(display, scores, shortcuts) {
-        this.fastKeys   = [37, 65, 40, 83, 39, 68];
+        // Các phím mũi tên và các phím khác
+        this.fastKeys   = [37, 65, 40, 83, 39, 68, 38, 87]; // Mũi tên trái (37), mũi tên lên (38), mũi tên xuống (40), mũi tên phải (39), W (87), A (65), S (83), D (68)
         this.shortcuts  = shortcuts;
         this.keyPressed = null;
         this.count      = 0;
@@ -37,7 +38,8 @@ class Keyboard {
             if (!this.display.isPlaying()) {
                 event.preventDefault();
             }
-            
+
+            // Gán phím theo các mã phím và hành động tương ứng
             if ([8, 66, 78].indexOf(key) > -1) {            // Backspace / B / N
                 key = "B";
             } else if ([13, 79, 84].indexOf(key) > -1) {    // Enter / O / T
@@ -46,11 +48,11 @@ class Keyboard {
                 key = "P";
             } else if ([17, 32].indexOf(key) > -1) {        // Ctrl / Space
                 key = "C";
-            } else if ([38, 87].indexOf(key) > -1) {        // Up    / W
+            } else if ([38, 87].indexOf(key) > -1) {        // Up / W
                 key = "W";
-            } else if ([37, 65].indexOf(key) > -1) {        // Left  / A
+            } else if ([37, 65].indexOf(key) > -1) {        // Left / A
                 key = "A";
-            } else if ([40, 83].indexOf(key) > -1) {        // Down  / S
+            } else if ([40, 83].indexOf(key) > -1) {        // Down / S
                 key = "S";
             } else if ([39, 68].indexOf(key) > -1) {        // Right / D
                 key = "D";
@@ -64,7 +66,7 @@ class Keyboard {
                 }
                 key = String.fromCharCode(key);
             }
-            
+
             if (number !== null) {
                 this.shortcuts.number(number);
             }
@@ -77,9 +79,9 @@ class Keyboard {
     onKeyDown(event) {
         const currentTime = Date.now();  // Lấy thời gian hiện tại
 
-        // Kiểm tra nếu phím mũi tên được nhấn và thời gian giữa hai lần bấm phím mũi tên lớn hơn 200ms
+        // Kiểm tra nếu phím mũi tên được nhấn và thời gian giữa hai lần bấm phím mũi tên lớn hơn 100ms
         if (this.display.isPlaying() && this.fastKeys.indexOf(event.keyCode) > -1) {
-            // Kiểm tra nếu thời gian giữa hai lần bấm phím mũi tên phải/trái quá ngắn (dưới 200ms)
+            // Kiểm tra nếu thời gian giữa hai lần bấm phím mũi tên phải/trái quá ngắn (dưới 100ms)
             if (currentTime - this.lastKeyPressTime > 100) {
                 this.lastKeyPressTime = currentTime;  // Cập nhật thời gian bấm phím
                 this.keyPressed = event.keyCode;  // Lưu lại phím được nhấn lần đầu
